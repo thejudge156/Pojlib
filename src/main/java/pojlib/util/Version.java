0fd5e1a2054779b0ecbe1b80a15e76d35612a16b
The Judge <colepatirckroberts@gmail.com>
2024-10-28 19:21:27 -0700

    Use latest fabric version
    The "stable" flag doesn't actually mean "stable," it just means recommended

---
+++ src/main/java/pojlib/util/Version.java
@@ -0,0 +1,11 @@
+package pojlib.util;
+
+public class Version {
+    public final int major, minor, patch;
+
+    public Version(int major, int minor, int patch) {
+        this.major = major;
+        this.minor = minor;
+        this.patch = patch;
+    }
+}
