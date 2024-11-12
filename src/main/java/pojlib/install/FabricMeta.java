6c6ce76c7b9cb116964fba34462e2743e899913a
The Judge <colepatirckroberts@gmail.com>
2024-11-11 23:01:27 -0800

    Fix fabric

--- src/main/java/pojlib/install/FabricMeta.java
+++ src/main/java/pojlib/install/FabricMeta.java
@@ -3,6 +3,7 @@ package pojlib.install;
 import com.google.gson.annotations.SerializedName;
 import pojlib.APIHandler;
 import pojlib.util.Constants;
+import pojlib.util.Version;
 
 public class FabricMeta {
 
@@ -13,17 +14,54 @@ public class FabricMeta {
         public String version;
         @SerializedName("stable")
         public boolean stable;
+        @SerializedName("separator")
+        public String separator;
     }
 
     public static FabricVersion[] getVersions() {
         return handler.get("versions/loader", FabricVersion[].class);
     }
 
-    public static FabricVersion getLatestStableVersion() {
-        for (FabricVersion version : getVersions()) {
-            if (version.stable) return version;
+    private static Version getVersionFromFabric(FabricVersion fabric) {
+        if(fabric.separator.contains("+")) {
+            // Only used pre-0.11, no use for us
+            return null;
         }
-        return null;
+
+        String[] verName = fabric.version.split("\\.");
+        if(verName.length < 3) {
+            return null;
+        }
+        int major = Integer.parseInt(verName[0]);
+        int minor = Integer.parseInt(verName[1]);
+        int patch = Integer.parseInt(verName[2]);
+
+        return new Version(major, minor, patch);
+    }
+
+    public static FabricVersion getLatestVersion() {
+        FabricVersion latest = null;
+        for (FabricVersion version : getVersions()) {
+            if(latest == null) {
+                latest = version;
+                continue;
+            }
+
+            Version newVer = getVersionFromFabric(version);
+            Version latestVer = getVersionFromFabric(latest);
+
+            if(newVer == null || latestVer == null)
+                continue;
+
+            if(latestVer.major < newVer.major) {
+                latest = version;
+            } else if(latestVer.major == newVer.major && latestVer.minor < newVer.major) {
+                latest = version;
+            } else if(latestVer.major == newVer.major && latestVer.minor == newVer.major && latestVer.patch < newVer.patch) {
+                latest = version;
+            }
+        }
+        return latest;
     }
 
     public static VersionInfo getVersionInfo(FabricVersion fabricVersion, String minecraftVersion) {
